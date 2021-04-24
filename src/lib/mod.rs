970d2e8355732ff542fa15d6bfe4e21412657461
fuyutarow <fuyutarow@gmail.com>
2021-04-24 14:41:28 +0900

    Support for neosnippet format

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use serde_derive::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::ffi::OsStr;
@@ -77,11 +78,13 @@ impl Config {
             .expect("something went wrong reading the file");
         Self::from_toml(&contents)
     }
+
     fn from_toml(toml_str: &str) -> Self {
         let rawConfig = toml::from_str::<RawConfig>(toml_str).expect("failed to parse config file");
         Self::from(rawConfig)
     }
-    pub fn to_json(&self) -> String {
+
+    pub fn to_vscode(&self) -> String {
         let vscode_snippet_map: HashMap<String, VSCodeSnippet> = self
             .clone()
             .snippets
@@ -90,5 +93,48 @@ impl Config {
             .collect();
 
         serde_json::to_string(&vscode_snippet_map).expect("failed to format config to json")
+    }
+
+    pub fn print_vscode(&self) {
+        println!("{}", self.to_vscode());
+    }
+
+    pub fn write_vscode(&self, editor: &str) -> anyhow::Result<PathBuf> {
+        match get_vscode_path(editor) {
+            Some(vscode_path) => {
+                let mut f = fs::File::create(&vscode_path).expect("failed to create file");
+                f.write_all(self.to_vscode().as_bytes())
+                    .expect("failed to write file");
+                Ok(vscode_path)
+                // );
+            }
+            _ => Err(anyhow::anyhow!("")),
+        }
+    }
+
+    pub fn print_neosnippet(&self) {
+        for (name, dot_snippet) in self.clone().snippets.into_iter() {
+            println!("snippet {}", name);
+            for line in dot_snippet.body.lines() {
+                println!("    {}", line);
+            }
+            println!("");
+        }
+    }
+}
+
+fn get_vscode_path(vscode: &str) -> Option<PathBuf> {
+    if let Some(config_path) = dirs::config_dir() {
+        match os_info::get().os_type() {
+            os_info::Type::Macos if vscode == "vscode" => {
+                Some(config_path.join("Code/User/snippets/typescript.json"))
+            }
+            os_info::Type::Macos if vscode == "vscode-insiders" => {
+                Some(config_path.join("Code - Insiders/User/snippets/typescript.json"))
+            }
+            _ => None,
+        }
+    } else {
+        None
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,15 @@ use lib::Config;
 
 #[derive(StructOpt, Debug)]
 struct Opt {
-    /// input toml file
+    /// Input toml file
     #[structopt(parse(from_os_str,))]
     fpath: PathBuf,
 
-    /// snippet format of output. [vscode]
-    #[structopt(short = "-f", long = "--format", default_value = "vscode")]
-    format: String,
+    /// Snippet format of output. [possible values: vscode, neosnippet]
+    #[structopt(short = "-t", long = "--to", default_value = "vscode")]
+    to: String,
 
-    /// overwrite config of editor. [vscode, vscode-insiders]
+    /// Overwrite config of editor. [vscode, vscode-insiders]
     #[structopt(long)]
     over: Option<String>,
     //
@@ -24,67 +24,36 @@ struct Opt {
     // out: Option<PathBuf>,
 }
 
-fn get_vscode_path(vscode: &str) -> Option<PathBuf> {
-    if let Some(config_path) = dirs::config_dir() {
-        match os_info::get().os_type() {
-            os_info::Type::Macos if vscode == "vscode" => {
-                Some(config_path.join("Code/User/snippets/typescript.json"))
-            }
-            os_info::Type::Macos if vscode == "vscode-insiders" => {
-                Some(config_path.join("Code - Insiders/User/snippets/typescript.json"))
-            }
-            _ => None,
-        }
-    } else {
-        None
-    }
-}
-
 fn main() {
     match Opt::from_args() {
         Opt {
             fpath,
-            format,
+            to,
             // out,
             over,
-        } => match format.as_str() {
-            "vscode" => {
-                let config = Config::from_fpath(fpath);
+        } => {
+            let config = Config::from_fpath(fpath);
 
-                let res = config.to_json();
-
-                match &over {
-                    Some(editor) if editor == "vscode" || editor == "vscode-insiders" => {
-                        match get_vscode_path(&editor) {
-                            Some(vscode_path) => {
-                                let mut f =
-                                    fs::File::create(&vscode_path).expect("failed to create file");
-                                f.write_all(res.as_bytes()).expect("failed to write file");
-                                println!(
-                                    "{} was overwritten",
-                                    vscode_path.into_os_string().into_string().expect("")
-                                );
-                            }
-                            _ => {
-                                println!("this os is not suppored");
-                            }
+            match to.as_str() {
+                "vscode" => {
+                    if let Some(editor) = over {
+                        let result = config.write_vscode(&editor);
+                        if let Ok(vscode_path) = result {
+                            println!(
+                                "{} was overwritten",
+                                vscode_path.into_os_string().into_string().expect("")
+                            );
+                        } else {
+                            println!("{} is not a suppored editor. [possible value: vscode, vscode-insiders]", editor);
                         }
-                    }
-                    Some(editor) => {
-                        println!(
-                            "{} is not a supported editor. Only [vscode, vscode-insiders]",
-                            editor
-                        );
-                    }
-                    _ => {
-                        println!("{}", res);
+                    } else {
+                        config.print_vscode();
                     }
                 }
+                "neosnippet" => config.print_neosnippet(),
+                _ => {}
             }
-            _ => {
-                println!("vscode is only supported, currently");
-            }
-        },
+        }
         _ => {}
     }
 }
